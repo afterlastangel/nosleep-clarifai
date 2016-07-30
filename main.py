44d951373a0165dc387c9352b552283bb327c95b
Le Kien Truc <truc.le@trustcircleglobal.com>
2016-07-30 15:50:08 +0700

    new

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask, request, jsonify
+from clarifai.client import ClarifaiApi
+
+os.environ['CLARIFAI_APP_ID'] = 'sTqW-xHoMnvCcxyJAXWXbZxe3XOxtzYfsik4A218'
+os.environ['CLARIFAI_APP_SECRET'] = '0OACpD2GFomsFNVzO80uFqQDvXzSX2nVAExCe-d_'
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def proxy():
+    image_url = request.args.get('image_url')
+    clarifai_api = ClarifaiApi(
+        model='general-v1.3')  # assumes environment variables are set.
+    result = clarifai_api.tag_image_urls(image_url)
+    return jsonify(result)
+
+if __name__ == "__main__":
+        app.run()
